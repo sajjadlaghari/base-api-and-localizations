8de3627b2c6e87ad6a91ba564055ff04e84b4d5e
Sajjad Laghari <sajjadlaghari723@gmail.com>
2025-01-02 22:01:36 +0500

    Permission Added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,13 +7,42 @@ use App\Models\User;
 use App\Models\UserLocalization;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
     public function index()
     {
+
+        // Get all permission
+        $permissions = Permission::all();
+
+        // $adminRole = Role::where('name', 'admin')->first();
+        $adminRole = Role::where('name', 'writer')->first();
+
+        // Assign all permissions to the role
+        $adminRole->syncPermissions($permissions);
+
+        
+        $user = User::find(1); // Fine User
+        $user->syncRoles('writer');  // Add New role
+        // $user->syncRoles([]);  // for remove all roles from current  User
+
+
+        $user = User::find(1); // Replace `1` with the user ID
+        $permissions = $user->getAllPermissions();
+
+        if ($user->can('create-user'))
+        {
+            echo "<p>You are an admin!</p>";
+
+        }
+
         $users = User::all();
-        return view('users.index',['users' => $users]);
+        $permissionsGroupedByGroupName = Permission::all()->groupBy('group_name');
+
+        return view('users.index',['users' => $users,'permissionsGroupedByGroupName' => $permissionsGroupedByGroupName]);
     }
 
     public function add_form()
